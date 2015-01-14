384a984453fd03c6ed3dd46311f69c7a97d2180f
Zhang, Shulai <kikyou1986@hotmail.com>
2015-01-14 10:16:06 +0800

    updating

---
+++ src/main/java/org/ikgroup/controller/LoginController.java
@@ -0,0 +1,18 @@
+package org.ikgroup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@RequestMapping("/login")
+@Controller
+public class LoginController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String show(Model uiModel){
+		return "login/index";
+	}
+
+}
